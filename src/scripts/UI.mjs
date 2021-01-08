9e92e1b2c5811f4e746c425b083547db8903186b
Jakub Jaruszewski <kuba10j@gmail.com>
2021-01-08 21:41:44 +0100

    folder update && ui update

--- src/scripts/UI.mjs
+++ src/scripts/UI.mjs
@@ -2,6 +2,33 @@ const NavBarTrigger = 60;
 
 
 function init() {
+    prapareNavigation();
+    preventOutline();
+}
+
+function preventOutline() {
+    document.addEventListener("mousedown", e => {
+        if (e.button !== 0) return;
+        let t = e.target;
+        while (t.parentNode) {
+            if (!t.hasAttribute("tabindex")) t = t.parentNode;
+            else break;
+        }
+        if (t == document || t == document.body) return;
+        let s = t.style.outline;
+        t.style.outline = "none";
+        t.addEventListener(
+            "blur",
+            () => {
+                t.style.outline = s;
+            }, {
+                once: true,
+            }
+        );
+    });
+}
+
+function prapareNavigation() {
     //navbar Scroll
     const navBar = document.getElementById("navBar");
     let navBarTick = false;
@@ -27,4 +54,6 @@ function init() {
         sidebarContainer.classList.toggle("open");
     });
 }
-export default { init }
+export default {
+    init
+}
