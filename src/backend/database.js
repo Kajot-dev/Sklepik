abb143da14051dcb4cd24f41e01d88010eaf16f2
Jakub Jaruszewski <kuba10j@gmail.com>
2021-01-23 20:40:03 +0100

    port update

--- src/backend/database.js
+++ src/backend/database.js
@@ -8,6 +8,6 @@ const configPath = path.join(databasePath, "config.json")
 
 //config
 const config = low(new FileSync(configPath));
-config.defaults({ sessionSecret: "xyz", httpPort: 8000, httpsPort: 8001 }).write();
+config.defaults({ sessionSecret: "xyz", httpPort: 80}).write();
 
 module.exports = { config };
