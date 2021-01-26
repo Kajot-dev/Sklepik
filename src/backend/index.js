9eb59ba9e1e4613df57465bcfa85dc5f96a3d123
Jakub Jaruszewski <kuba10j@gmail.com>
2021-01-26 10:50:54 +0100

    s

--- src/backend/index.js
+++ src/backend/index.js
@@ -7,6 +7,7 @@ const cluster = require("cluster");
 const colors = require("colors");
 const worker = require("./worker");
 const helmet = require("helmet");
+const path = require("path");
 const {
     config
 } = require("./database");
@@ -15,46 +16,49 @@ const httpPort = process.env.PORT || 8000;
 
 async function init() {
     if (cluster.isMaster) {
-        
-        
-        for (let i = 0; i < os.cpus().length; i++) {
-            cluster.fork({ id: i+1 });
-        }
-
-        console.log("Worker is starting!");
-        worker.init();
-    } else {
         const app = express();
-    
+
         app.use(helmet({
             contentSecurityPolicy: {
-              directives: Object.assign(helmet.contentSecurityPolicy.getDefaultDirectives(), {
-                frameSrc: ["'self'", "https://www.google.com/"],
-              })
+                directives: Object.assign(helmet.contentSecurityPolicy.getDefaultDirectives(), {
+                    frameSrc: ["'self'", "https://www.google.com/"],
+                })
             }
-          }));
+        }));
+        const sessionSecret = (await config).get("sessionSecret").value();
+        const sessionName = (await config).get("sessionName").value();
+        console.log(sessionName, sessionSecret);
         app.use(session({
-            secret: (await config).get("sessionSecret").value(),
+            secret: sessionSecret,
             saveUninitialized: true,
-            resave: true,
-            name: (await config).get("sessionName").value(),
+            resave: false,
+            name: sessionName,
             cookie: {
                 httpOnly: true,
                 sameSite: true,
                 maxAge: 1000 * 60 * 45,
-                signed: true
+                signed: true,
+                secure: true
             }
-            }));
+        }));
         app.use(bodyParser.urlencoded({
             extended: true
         }));
         app.use(bodyParser.json());
-    
+
         routes.init(app);
-    
+
         app.listen(httpPort);
-        console.log(colors.magenta("HTTP") + " server thread " + colors.yellow(process.env.id) + " server is listening on port: "+colors.green(httpPort));
-        
+        console.log(colors.magenta("HTTP") + " server is listening on port: " + colors.green(httpPort));
+        cluster.fork();
+    } else {
+        console.log("Worker is starting!");
+        worker.init();
     }
+
+
+
+
+
 }
 init();
